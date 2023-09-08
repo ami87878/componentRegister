7fcd457b0bec008ec4ddeb932885ee164a74fb25
amirhossein khanmohammadi <amirhossein.kh.job@gmail.com>
2023-09-09 01:27:26 +0330

    #1 project that show register app

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>More on Vue Components</h1>
+  </header>
+</template>
+
+<style>
+  header {
+    width: 100%;
+    height: 5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #371f8d;
+  }
+
+  header h1 {
+    color: white;
+    margin: 0;
+  }
+</style>
