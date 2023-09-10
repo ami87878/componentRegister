59ad018122b081325617b8aaf4015bcf21834052
amirhossein khanmohammadi <amirhossein.kh.job@gmail.com>
2023-09-10 22:07:33 +0330

    add slot to decrease code  of component

--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,29 +1,43 @@
 <template>
   <section>
-    <div>
+    <BaseCard>
+    <header>
+    
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
-    </div>
-    <p>{{ infoText }}</p>
+    
+  </header>
+
+  <p>{{ infoText }}</p>
+</BaseCard>
+  
   </section>
 </template>
 
 <script>
+
+
+
+
+  
+import BaseCard from '../components/BaseCard.vue'
 export default {
+
+
+components:{
+
+  BaseCard,
+
+},
+
   props: ['fullName', 'infoText', 'role'],
 };
 </script>
 
-<style>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
+<style scoped>
 
-section div {
+
+section header {
   display: flex;
   justify-content: space-between;
   align-items: center;
