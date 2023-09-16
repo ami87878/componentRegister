1fc1e5d544cf1042bcf01351b58085ad6677f49e
Amirhossein <ak47.amirhosseinkhanmohamadi@gmail.com>
2023-09-16 23:15:31 +0330

    #9 add new componet

---
+++ src/components/CourseGoals.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul>
+        
+    </ul>
+</template>
+
+<script>
+export default {
+    name: 'CourseGoals',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
