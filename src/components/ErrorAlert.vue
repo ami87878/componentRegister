a099e2bb8c208faefed71bf409a7aef11ab817ed
Amirhossein <ak47.amirhosseinkhanmohamadi@gmail.com>
2023-09-18 00:19:41 +0330

    wanna have modal with slot witout aleert

---
+++ src/components/ErrorAlert.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <dialog>
+            <slot></slot> <!--content  is flexible-->
+        </dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ErrorAlert',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style scoped>
+
+dialog{
+
+
+    position: fixed;
+    top: 20vh;
+    left: 30%;
+    width: 40%;
+    background-color: white;
+    box-shadow: 0 2px 8px rgb(0, 0, 0.26);
+    padding: 1rem;
+}
+
+</style>
--- src/components/ManageGoals.vue
+++ src/components/ManageGoals.vue
@@ -1,7 +1,63 @@
 <template>
- <div>
-    <h2>Manage Goals</h2>
-    <input type="text" name="" id="">
+    <div>
+        <div>
+            <h2>Manage Goals</h2>
+            <input type="text" name="" id="" ref="goal">
+            <button  @click="setGoal" >set goal</button>
+            <ErrorAlert v-if="inputIsInValid">
+
+                <h2 >input is invalid</h2>
+             
+
+            </ErrorAlert>
+           
 </div>
+    </div>
 </template>
 
+<script>
+import ErrorAlert from './ErrorAlert.vue'
+export default {
+
+
+    components:{
+
+        ErrorAlert,
+
+
+    },
+    name: 'ManageGoals',
+
+    data() {
+        return {
+           inputIsInValid:false,
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+
+
+    setGoal(){
+
+        const enteredValue=this.$refs.goal.value;
+
+        if(enteredValue===''){
+
+           this.inputIsInValid=true
+
+        }
+
+
+    }        
+        
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
