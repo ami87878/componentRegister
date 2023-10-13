a034c0b46e3269eee022f4ed5b82ba2829a56bbc
amirhossein <ak47.amirhosseinkhanmohamadi@gmail.com>
2023-10-13 17:31:25 +0330

    new feature add content in  AddResourse component

--- src/components/AddResourses.vue
+++ src/components/AddResourses.vue
@@ -1,5 +1,33 @@
 <template>
-    <h2>Add Resourses</h2>
+    <base-card>
+        <div class="form-control">
+            <form>
+                <label for="v">Title</label>
+                    <input  id='title' type="text" name="title">
+                
+            </form>
+        </div>
+        
+        <div class="form-control">
+            <form>
+                <label for="Describtion">Describtion </label>
+                <textarea name="Describtion" id="Describtion"  rows="3"></textarea>
+                
+            </form>
+        </div>
+        <div class="form-control">
+            <form>
+                <label for="Link">Link </label>
+                <input name="Link" id="Link" type="url" />
+                
+            </form>
+        </div>
+
+        <base-button type="submit"> Add Resourse</base-button>
+
+
+
+    </base-card>
 </template>
 
 <script>
@@ -23,5 +51,32 @@ export default {
 </script>
 
 <style scoped>
+
+label {
+  font-weight: bold;
+  display: block;
+  margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  padding: 0.15rem;
+  border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+  outline: none;
+  border-color: #3a0061;
+  background-color: #f7ebff;
+}
+
+.form-control {
+  margin: 1rem 0;
+}
+
 
 </style>
