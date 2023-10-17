359b5092ab2d7bb487195e2d44cefae5b24d9a4d
amirhossein <ak47.amirhosseinkhanmohamadi@gmail.com>
2023-10-17 21:50:31 +0330

    change: Addresourse component order front-end

--- src/components/AddResourses.vue
+++ src/components/AddResourses.vue
@@ -1,29 +1,32 @@
 <template>
     <base-card>
-        <div class="form-control" @submit.prevent="submitData">
-            <form>
-                <label for="v">Title</label>
+    <form  @submit.prevent="submitData">
+        <div class="form-control" >
+                <label for="title">Title</label>
                     <input  id='title' type="text" name="title" ref="titleInput" >
                 
-            </form>
-        </div>
-        
-        <div class="form-control">
-            <form>
-                <label for="Describtion">Describtion </label>
-                <textarea name="Describtion" id="Describtion"  rows="3" ref="DescribtionInput" ></textarea>
+                </div>
                 
-            </form>
-        </div>
+                <div class="form-control">
+            
+                        <label for="Describtion">Describtion </label>
+                        <textarea name="Describtion" id="Describtion"  rows="3" ref="DescribtionInput" ></textarea>
+                        
+            
+                  </div>
+
         <div class="form-control">
-            <form>
+            
                 <label for="Link">Link </label>
                 <input name="Link" id="Link" type="url"  ref="LinkInput"/>
                 
-            </form>
+        
         </div>
 
-        <base-button type="submit"> Add Resourse</base-button>
+                   <div>
+                        <base-button type="submit"> Add Resourse</base-button>
+                 </div>
+    </form>
 
 
 
@@ -34,7 +37,7 @@
 export default {
     name: 'AddResourses',
 
-    inject:['addResourceTochild'],
+    inject:['addResource'],
     data() {
         return {
           
@@ -53,8 +56,15 @@ export default {
             const titleDta=this.$refs.titleInput.value;
             const DescribtionData=this.$refs.DescribtionInput.value;
             const Linkdata=this.$refs.LinkInput.value;
-            
-            this.addResourceTochild(titleDta,DescribtionData,Linkdata),
+
+            if(titleDta.trim()===''||DescribtionData.trim()===''|| Linkdata.trim()===''){
+
+            <base-dialog></base-dialog>
+
+                return;
+
+            }
+            this.addResource(titleDta,DescribtionData,Linkdata);
 
         }
         
